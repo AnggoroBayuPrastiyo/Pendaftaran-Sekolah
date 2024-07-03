a0e5bc79ab317480a935b4fedc4dd2864af8e294
Putrikhodi <168696443+Putrikhodi@users.noreply.github.com>
2024-07-03 15:06:09 +0700

    lihat hasil seleksi 1 kali

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,11 +3,11 @@
 class User_model extends CI_Model {
     public function get_user($user_id) {
         $this->db->where('id', $user_id);
-        return $this->db->get('users')->row();
+        return $this->db->get('user')->row();
     }
 
     public function mark_result_as_seen($user_id) {
         $this->db->where('id', $user_id);
-        $this->db->update('users', array('has_seen_result' => 1));
+        $this->db->update('user', array('has_seen_result' => 1));
     }
 }
