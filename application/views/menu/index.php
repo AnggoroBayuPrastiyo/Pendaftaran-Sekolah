72c6f848b25ac6b346a66326feb55901a39fbcec
Anggoro Bayu Prastiyo <anggorobayu,prastiyo12@gmail.com>
2024-07-05 01:45:36 +0700

    test

--- application/views/menu/index.php
+++ application/views/menu/index.php
@@ -1,53 +1,84 @@
-<!-- application/views/menu/index.php -->
+<!-- Begin Page Content -->
 <div class="container-fluid">
-    <h1 class="h3 mb-4 text-gray-800">Registered Users</h1>
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="table-responsive"> <!-- Tambahkan wrapper table-responsive -->
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Nomor Pendaftaran</th>
-                            <th>Nama Peserta</th>
-                            <th>Tempat Lahir</th>
-                            <th>Tanggal Lahir</th>
-                            <th>Jenis Kelamin</th>
-                            <th>Nomor Akta</th>
-                            <th>NIK</th>
-                            <th>Alamat</th>
-                            <th>Nama Wali</th>
-                            <th>Nomor HP</th>
-                            <th>Ijazah</th>
-                            <th>Kartu Keluarga</th>
-                            <th>Akta Kelahiran</th>
-                            <th>KTP</th>
-                            <!-- Tambahkan kolom lain sesuai kebutuhan -->
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($registrations as $registration): ?>
-                        <tr>
-                            <td><?php echo $registration['nomor_pendaftaran']; ?></td>
-                            <td><?php echo $registration['nama_peserta']; ?></td>
-                            <td><?php echo $registration['tempat_lahir']; ?></td>
-                            <td><?php echo $registration['tanggal_lahir']; ?></td>
-                            <td><?php echo $registration['jenis_kelamin']; ?></td>
-                            <td><?php echo $registration['nomor_akta']; ?></td>
-                            <td><?php echo $registration['nik']; ?></td>
-                            <td><?php echo $registration['alamat']; ?></td>
-                            <td><?php echo $registration['nama_wali']; ?></td>
-                            <td><?php echo $registration['nomor_hp']; ?></td>
-                            <td><a href="<?php echo base_url('uploads/' . $registration['ijazah']); ?>" download>Download</a></td>
-                            <td><a href="<?php echo base_url('uploads/' . $registration['kk']); ?>" download>Download</a></td>
-                            <td><a href="<?php echo base_url('uploads/' . $registration['akte']); ?>" download>Download</a></td>
-                            <td><a href="<?php echo base_url('uploads/' . $registration['ktp']); ?>" download>Download</a></td>
-   
-                            <!-- Tambahkan kolom lain sesuai kebutuhan -->
-                        </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+
+
+  <!-- Page Heading -->
+  <h1 class="h3 mb-4 text-gray-800"><?= $title;?></h1>
+
+
+  <div class="row">
+    <div class="col-lg-6">
+      <?= form_error('menu','<div class="alert alert-danger" role="alert">','</div>')?>
+
+      <?= $this->session->flashdata('message')?>
+
+
+
+      <a href="" class="btn btn-primary mb-4" data-toggle="modal" data-target="#exampleModal">Add New Menu</a>
+
+
+
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Menu</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php  $i =1;?>
+          <?php foreach((array)$menu as $m) :?>
+          <tr>
+            <th scope="row"><?= $i;?></th>
+            <td><?= $m['menu']; ?></td>
+            <td>
+              <a href="" class="badge badge-success">edit</a>
+              <a href="" class="badge badge-danger">delete</a>
+
+            </td>
+          <tr>
+            <?php $i++?>
+            <?php endforeach; ?>
+        </tbody>
+
+      </table>
     </div>
+  </div>
+
+
+  <a href="<?= base_url('registration') ?>">Link ke registration</a>
+  <a href="<?= base_url('seleksi') ?>">Link ke seleksi</a>
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add New Menu</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <form action="<?= base_url('menu')?>" method="post">
+        <div class="modal-body">
+          <div class="form-group">
+            <input type="text" class="form-control" id="menu" name="menu" placeholder="Menu Name">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Add</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </div>
