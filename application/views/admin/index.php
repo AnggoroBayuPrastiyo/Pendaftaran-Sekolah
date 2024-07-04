8dc578dcb8b93387de02e1847449cb1f04b6e99d
Anggoro Bayu Prastiyo <anggorobayu,prastiyo12@gmail.com>
2024-07-05 05:47:47 +0700

    memperbaiki bug

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,18 +1,9 @@
-<!-- Begin Page Content -->
-<div class="container-fluid">
-
-  <!-- Page Heading -->
-  <h1 class="h3 mb-4 text-gray-800"><?= $title;?></h1>
-
-
-  <a href="<?= base_url('registration') ?>">Link ke registration</a>
-  <a href="<?= base_url('seleksi') ?>">Link ke seleksi</a>
-
-
-
-
+<div class="container-fluid mt-4">
+  <div class="jumbotron">
+    <h1 class="display-4">Selamat Datang di Dashboard Admin SMAN Ceria!</h1>
+    <p class="lead">Anda memiliki akses penuh untuk mengelola data siswa, penerimaan siswa, dan role pengguna.</p>
+    <hr class="my-4">
+    <p>Silakan gunakan menu di atas untuk navigasi cepat ke halaman yang Anda butuhkan.</p>
+    <a class="btn btn-primary btn-lg" href="<?= base_url('admin/tabelsiswa') ?>" role="button">Lihat Tabel Siswa</a>
+  </div>
 </div>
-<!-- /.container-fluid -->
-
-</div>
-<!-- End of Main Content -->
