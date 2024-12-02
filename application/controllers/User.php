8dbffd565729455319565c64a23cb3bea17c6dac
Anggoro Bayu Prastiyo <anggorobayu.prastiyo12@gmail.com>
2024-12-02 16:21:31 +0700

    menambahkan dashboard untuk siswa

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -178,4 +178,19 @@ class User extends CI_Controller
     $this->load->view('templates/footer');
   }
 
+  public function dashboard()
+  {
+
+        $data['title'] = 'Dashboard';
+        $data['user'] = $this->db->get_where('user', ['email'=> $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('user/dashboard', $data);
+        $this->load->view('templates/footer');
+    }
+
+  
+
 }
---
+++ application/views/user/dashboard.php
@@ -0,0 +1,11 @@
+<div class="container-fluid mt-4">
+  <div class="jumbotron">
+    <h1 class="display-4">Selamat Datang di Dashboard Siswa SMAN Ceria!</h1>
+    <p class="lead">Ini adalah tempat untuk mengakses informasi pribadi Anda, jadwal pelajaran, dan pengumuman penting.
+    </p>
+    <hr class="my-4">
+    <p>Gunakan menu di atas untuk mengakses halaman yang Anda butuhkan.</p>
+    <a class="btn btn-primary btn-lg" href="<?= base_url('siswa/profil') ?>" role="button">Lihat Profil Saya</a>
+    <a class="btn btn-secondary btn-lg" href="<?= base_url('siswa/jadwal') ?>" role="button">Lihat Jadwal Pelajaran</a>
+  </div>
+</div>
